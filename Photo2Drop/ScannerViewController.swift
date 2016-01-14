37ce2134d758d03b8a26cddd14ea88ca0385f345
pkang9936 <kang.puthyrak@gmail.com>
2016-01-13 22:11:34 -0600

    Update

--- Photo2Drop/ScannerViewController.swift
+++ Photo2Drop/ScannerViewController.swift
@@ -7,13 +7,79 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ScannerViewController: SWFrontViewController {
+class ScannerViewController: SWFrontViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var session: AVCaptureSession!
+    var theCamera: AVCaptureDevice!
+    var theInputSource: AVCaptureDeviceInput!
+    var theOutputSource: AVCaptureMetadataOutput!
+    var thePreview: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let allCameras = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        
+        for camera in allCameras {
+            if camera.position == AVCaptureDevicePosition.Back {
+                theCamera = camera as! AVCaptureDevice
+                break
+            }
+        }
+        session = AVCaptureSession()
+        if theCamera != nil {
+            do {
+                try theInputSource = AVCaptureDeviceInput(device: theCamera)
+                
+            }catch _ as NSError {
+                NSLog("Error")
+            }
+            if session.canAddInput(theInputSource){
+                session.addInput(theInputSource)
+            }
+            
+            theOutputSource = AVCaptureMetadataOutput()
+            if session.canAddOutput(theOutputSource) {
+                session.addOutput(theOutputSource)
+            }
+            let options = [AVMetadataObjectTypeQRCode,
+                AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypePDF417Code
+            ]
+            
+            theOutputSource.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            theOutputSource.metadataObjectTypes = options
+            
+            thePreview = AVCaptureVideoPreviewLayer(session: session)
+            self.view.layer.addSublayer(thePreview)
+            thePreview.frame = self.view.bounds
+            thePreview.videoGravity = AVLayerVideoGravityResizeAspectFill
+            
+        }
+        session.startRunning()
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        for theItem in metadataObjects {
+            if let _item = theItem as? AVMetadataMachineReadableCodeObject {
+                print("We read\(_item.stringValue) from a barcode of type: \(_item.type)")
+            }
+        }
     }
     @IBAction func doScanBarCode(sender: AnyObject) {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
